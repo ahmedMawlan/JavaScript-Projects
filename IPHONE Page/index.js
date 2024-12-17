875ee8fc3fda3a77d5ea5cb7f5d349d7e1f568a4
Ahmed Ramadan <elroby480@gmail.com>
2024-12-17 13:26:39 +0200

    first commit

---
+++ IPHONE Page/index.js
@@ -0,0 +1,10 @@
+let image = document.getElementById('image')
+let container = document.body
+
+function myFunction(src){
+    image.src = src
+}
+
+function colors(color){
+    container.style.background = color
+}
